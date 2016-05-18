b23c98363fc8a358064ffd03f6f54ddd8ff9a336
theskillwithin <austinppeterson@gmail.com>
2016-05-18 00:01:01 -0700

    Start

--- html/app/code/Austinpeterson/Commercebug/Plugin/Magento/Framework/View/Layout.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Austinpeterson\Commercebug\Plugin\Magento\Framework\View;
-class Layout
-{
-    function beforeGenerateElements($subject){
-        \Austinpeterson\Commercebug\Model\All::addTo(
-            'request_layout_xml', $subject->getNode()->asXml());
-    }
-}
