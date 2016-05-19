94c364151be0cc708c68e0a4de52fd4c98854205
theskillwithin <austinppeterson@gmail.com>
2016-05-18 21:06:18 -0700

    update registration

--- html/app/design/frontend/brandastic/intelliamor/registration.php
+++ html/app/design/frontend/brandastic/intelliamor/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Snowdog/blank',
+    'frontend/brandastic/intelliamor',
     __DIR__
 );
